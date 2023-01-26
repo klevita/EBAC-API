3cd11ca523b953291c6827360f7f02a789cf36f9
klevita <kklevita92@gmail.com>
2023-01-26 14:41:21 +0300

    db connection

--- dist/index.js
+++ dist/index.js
@@ -11,11 +11,12 @@ const body_parser_1 = __importDefault(require("body-parser"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const pool = new pg_1.default.Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'EBAC',
-    password: 'admin',
+    user: 'kklevita92',
+    host: 'ep-wispy-band-773803.eu-central-1.aws.neon.tech',
+    database: 'neondb',
+    password: '05zdWctCXuAf',
     port: 5432,
+    ssl: true,
 });
 var jsonParser = body_parser_1.default.json();
 app.use((0, cors_1.default)({
@@ -26,7 +27,7 @@ app.get('/', (req, res) => {
 });
 app.get('/task/getTasksByUserId/:id', (req, res) => {
     const id = req.params.id;
-    pool.query(`SELECT * FROM "testTask" WHERE "userId"=${id}`, (error, results) => {
+    pool.query(`SELECT * FROM ebac WHERE user_id=${id}`, (error, results) => {
         if (error) {
             throw error;
         }
@@ -38,24 +39,24 @@ app.post("/task/alterTask", jsonParser, function (req, res) {
     if (!((_a = req.body) === null || _a === void 0 ? void 0 : _a.userId) || !((_b = req.body) === null || _b === void 0 ? void 0 : _b.title) || ((_c = req.body) === null || _c === void 0 ? void 0 : _c.completed) === undefined)
         return res.sendStatus(400);
     const userId = req.body.userId;
-    const title = req.body.title;
     const completed = req.body.completed;
-    pool.query(`SELECT MAX(id)+1 FROM "testTask";`, (error, results) => {
+    let title;
+    if (req.body.title.length > 256) {
+        title = req.body.title.substring(0, 256);
+    }
+    else {
+        title = req.body.title;
+    }
+    pool.query(`INSERT INTO ebac (user_id, title, completed) VALUES(${userId}, '${title}', ${completed}) RETURNING id`, (error, results) => {
         if (error) {
             throw error;
         }
-        const nextId = Object.values(results.rows[0])[0];
-        pool.query(`INSERT INTO "testTask" (id, "userId",title,completed) VALUES(${nextId}, ${userId}, '${title}', ${completed}) RETURNING id`, (error, results) => {
-            if (error) {
-                throw error;
-            }
-            res.send(results.rows[0].id);
-        });
+        res.sendStatus(200);
     });
 });
 app.delete("/task/deleteTaskById/:id", function (req, res) {
     const id = req.params.id;
-    pool.query(`DELETE FROM "testTask" WHERE id=${id}`, (error, results) => {
+    pool.query(`DELETE FROM ebac WHERE id=${id}`, (error) => {
         if (error) {
             throw error;
         }
@@ -68,7 +69,7 @@ app.patch("/task/completeTaskById/:id", jsonParser, function (req, res) {
         return res.sendStatus(400);
     const completed = req.body.completed;
     const id = req.params.id;
-    pool.query(`UPDATE "testTask" SET completed = ${completed} WHERE id = ${id}`, (error, results) => {
+    pool.query(`UPDATE ebac SET completed = ${completed} WHERE id = ${id}`, (error) => {
         if (error) {
             throw error;
         }
--- index.ts
+++ index.ts
@@ -7,11 +7,12 @@ import bodyParser from 'body-parser'
 dotenv.config();
 const app: Express = express();
 const pool: Pool = new pg.Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'EBAC',
-    password: 'admin',
+    user: 'kklevita92',
+    host: 'ep-wispy-band-773803.eu-central-1.aws.neon.tech',
+    database: 'neondb',
+    password: '05zdWctCXuAf',
     port: 5432,
+    ssl: true,
 })
 var jsonParser = bodyParser.json()
 
@@ -27,7 +28,7 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/task/getTasksByUserId/:id', (req: Request, res: Response) => {
     const id = req.params.id
-    pool.query(`SELECT * FROM "testTask" WHERE "userId"=${id}`, (error, results) => {
+    pool.query(`SELECT * FROM ebac WHERE user_id=${id}`, (error, results) => {
         if (error) {
             throw error
         }
@@ -37,26 +38,26 @@ app.get('/task/getTasksByUserId/:id', (req: Request, res: Response) => {
 
 app.post("/task/alterTask", jsonParser, function (req, res) {
     if (!req.body?.userId || !req.body?.title || req.body?.completed === undefined) return res.sendStatus(400);
+    
     const userId = req.body.userId;
-    const title = req.body.title;
     const completed = req.body.completed
-    pool.query(`SELECT MAX(id)+1 FROM "testTask";`, (error, results) => {
+    let title:string;
+    if(req.body.title.length>256){
+        title = req.body.title.substring(0,256)
+    }else{
+        title = req.body.title
+    }
+    pool.query(`INSERT INTO ebac (user_id, title, completed) VALUES(${userId}, '${title}', ${completed}) RETURNING id`, (error, results) => {
         if (error) {
             throw error
         }
-        const nextId = Object.values(results.rows[0])[0]
-        pool.query(`INSERT INTO "testTask" (id, "userId",title,completed) VALUES(${nextId}, ${userId}, '${title}', ${completed}) RETURNING id`, (error, results) => {
-            if (error) {
-                throw error
-            }
-            res.send(results.rows[0].id)
-        })
+        res.sendStatus(200)
     })
 });
 
 app.delete("/task/deleteTaskById/:id", function (req, res) {
     const id = req.params.id
-    pool.query(`DELETE FROM "testTask" WHERE id=${id}`, (error, results) => {
+    pool.query(`DELETE FROM ebac WHERE id=${id}`, (error) => {
         if (error) {
             throw error
         }
@@ -68,7 +69,7 @@ app.patch("/task/completeTaskById/:id", jsonParser, function (req, res) {
     if (req.body?.completed === undefined) return res.sendStatus(400);
     const completed = req.body.completed;
     const id = req.params.id
-    pool.query(`UPDATE "testTask" SET completed = ${completed} WHERE id = ${id}`, (error, results) => {
+    pool.query(`UPDATE ebac SET completed = ${completed} WHERE id = ${id}`, (error) => {
         if (error) {
             throw error
         }
